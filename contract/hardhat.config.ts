15df5e1015dac2e78e53da357e79608658580013
richelleji <ji.richelle@gmail.com>
2024-10-13 18:23:25 -0400

    change hardhat chainid to 1337
    to enable testing on metamask

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,11 +2,12 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
-    networks: {
-      hardhat: {
-        chainId: 1337
-      }
-},
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
+};
 
 export default config;
