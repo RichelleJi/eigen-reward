5678d3e4c71ef079facbf759725eaacbbece71e0
richelleji <ji.richelle@gmail.com>
2024-10-14 08:12:27 -0400

    Improve hardhat scripts

--- contract/script/deployEigen.js
+++ contract/script/deployEigen.js
@@ -1,13 +1,29 @@
+const fs = require('fs');
 const { ethers } = require('hardhat');
 
 async function main() {
-  const initialSupply = ethers.utils.parseEther('2'); // Set the initial supply (1 million tokens with 18 decimals)
-
+  const initialSupply = ethers.utils.parseEther('2');
   const Eigen = await ethers.getContractFactory('Eigen');
   const eigen = await Eigen.deploy(initialSupply);
 
   await eigen.deployed();
   console.log(`Eigen deployed at ${eigen.address}`);
+
+  const envFilePath = '../.env.local';
+  const addressLine = `NEXT_PUBLIC_EIGEN_CONTRACT_ADDRESS=${eigen.address}\n`;
+  
+
+  if (fs.existsSync(envFilePath)) {
+    let envFileContent = fs.readFileSync(envFilePath, 'utf8');
+    if (envFileContent.includes('NEXT_PUBLIC_EIGEN_CONTRACT_ADDRESS=')) {
+      envFileContent = envFileContent.replace(/NEXT_PUBLIC_EIGEN_CONTRACT_ADDRESS=.*/g, addressLine);
+    } else {
+      envFileContent += addressLine;
+    }
+    fs.writeFileSync(envFilePath, envFileContent);
+  } else {
+    fs.writeFileSync(envFilePath, addressLine);
+  }
 }
 
 main()
