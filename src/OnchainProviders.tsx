f5c62be701e214897086459f10d343067093fa69
richelleji <ji.richelle@gmail.com>
2024-10-14 08:18:35 -0400

    change provider to allow the access to sepolia testnet via rpc

--- src/OnchainProviders.tsx
+++ src/OnchainProviders.tsx
@@ -3,17 +3,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 import { WagmiProvider, createConfig, http } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia, localhost } from 'wagmi/chains';
+import { sepolia, localhost } from 'wagmi/chains';
 
 type Props = { children: ReactNode };
 
 const queryClient = new QueryClient();
 
 const config = createConfig({
-  chains: [mainnet, sepolia, localhost],
+  chains: [sepolia, localhost],
   transports: {
-    [mainnet.id]: http('https://mainnet.example.com'),
-    [sepolia.id]: http('https://sepolia.example.com'),
+    [sepolia.id]: http(),
     [localhost.id]: http('http://127.0.0.1:8545'),
   },
 });
