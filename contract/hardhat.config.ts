d890c352ad5b4f27761478733e390f28f84b97ce
richelleji <ji.richelle@gmail.com>
2024-10-14 08:14:01 -0400

    add sepolia network as a deployment location

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,11 +1,21 @@
 import type { HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+const { NEXT_PRIVATE_ALCHEMY_PRIVATE_KEY, NEXT_PRIVATE_WAlLLET_PRIVATE_KEY } =
+  process.env;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${NEXT_PRIVATE_ALCHEMY_PRIVATE_KEY}`,
+      accounts: [NEXT_PRIVATE_WAlLLET_PRIVATE_KEY],
     },
   },
 };
