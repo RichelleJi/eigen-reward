06e894009180d7c8c68c0c1924f702f429646be6
richelleji <ji.richelle@gmail.com>
2024-10-14 08:19:02 -0400

    removed unecessary files

--- src/utils/provider.ts
+++
@@ -1,9 +0,0 @@
-import { http, createPublicClient } from 'viem';
-import type { Chain } from 'viem/chains';
-
-export function getRpcProviderForChain(chain: Chain) {
-  return createPublicClient({
-    chain: chain,
-    transport: http(chain.rpcUrls.default.http[0]),
-  });
-}
